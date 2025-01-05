f50138a5779f81bbd065c5f6ad73c7072415fdcc
Kirsten Mork <kirstenmork@hotmail.com>
2025-01-05 13:44:08 -0800

    Server: Connect to DB
    Connect to db (using mongoose).
    
    Note: the password and db url must be in your .env file to be able
    to connect and use the server.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,28 @@
 import app from './app';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
-const port = process.env.PORT || 8000;
+// Set up connection to DB
+
+const databaseURL = process.env.DATABASE || '';
+const databasePassword = process.env.DATABASE_PASSWORD || '';
+if (!databaseURL || !databasePassword) {
+  throw new Error('Database URL or password not found - set in .env');
+}
+const DB = databaseURL.replace('<PASSWORD>', databasePassword);
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful');
+  })
+  .catch((err) => {
+    console.log('DB Error: ', err);
+  });
 
 // start the Express server
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });

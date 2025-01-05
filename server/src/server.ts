a800189ac2610533779a89703a830777d14895d4
Kirsten Mork <kirstenmork@hotmail.com>
2025-01-05 12:03:47 -0800

    Setup basic middleware
    Set up cors and express.json (to parse incoming payloads) middleware.
    
    Install type deps needed.

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,15 +1,22 @@
-import express, { Express, Request, Response } from 'express';
+import cors from 'cors';
 import dotenv from 'dotenv';
+import express, { Express, Request, Response } from 'express';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 8000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// test route
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
+// start the Express server
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });

edef207bfaddcc768a10cf31548a88766287ab8e
Kirsten Mork <kirstenmork@hotmail.com>
2025-01-05 12:20:22 -0800

    Hello World route for testing front end
    We can hit this endpoint on the frontend to make sure everything
    is connected.

--- server/src/app.ts
+++ server/src/app.ts
@@ -8,6 +8,9 @@ app.use(cors());
 app.use(express.json());
 
 // Mount Routers
+app.all('/message', (req, res, next) => {
+  res.send('Hello World');
+});
 app.all('*', (req, res, next) => {
   res.status(404).json({ status: 'fail', message: `Can't find ${req.url}` });
 });

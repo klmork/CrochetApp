3524af1a9cb66e6b69d14185ef1c08616f81e1b4
Kirsten Mork <kirstenmork@hotmail.com>
2025-01-05 22:49:22 -0800

    UI: Display Skeins
    Get the skeins from the API and display them.

--- client/src/pages/skeins.tsx
+++ client/src/pages/skeins.tsx
@@ -1,8 +1,26 @@
+import { useEffect, useState } from "react";
+import { Box, Text } from "@chakra-ui/react";
+import { SkeinProps, Skein } from "../components/Skeins/skein.tsx";
 const Skeins = () => {
+  const [skeins, setSkeins] = useState<SkeinProps[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/v1/skeins")
+      .then((res) => res.json())
+      .then((json) => {
+        const skeins: [] = json.data.skeinsWithImageUrl;
+        setSkeins(skeins);
+      });
+  }, []);
+  console.log("skeins", skeins);
+  //   console.log("id", skeins[0]._id, skeins[1]._id);
   return (
     <div className="container">
       <h1>My Yarn</h1>
-      <h3>WIP</h3>
+      {skeins &&
+        skeins.map((skein, i) => (
+          <Skein key={i} _doc={skein._doc} imageUrl={skein.imageUrl} />
+        ))}
     </div>
   );
 };

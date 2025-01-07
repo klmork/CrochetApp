46931d1f2129ca10f95abafb7d3d01c3c83bf84f
Kirsten Mork <kirstenmork@hotmail.com>
2025-01-06 21:43:43 -0800

    Skein Unit Test: test getAllSkeins []
    Make sure test set up is correct.
    
    Test getAllSkeins if the db were to return no skeins.
    - Stub out the db call (Skein.find) to return [].
    - request can be empty for this get
    - response works with our .status().json() call
    
    Don't need to stub S3 for this one since the array is empty (no
    urls to fetch).

--- server/__tests__/unit/controllers/skeinController.spec.ts
+++ server/__tests__/unit/controllers/skeinController.spec.ts
@@ -1,8 +1,32 @@
-// import { getAllSkeins, addSkien } from '../../controllers/skeinController';
-// import { Skein } from '../../models/skeinModel';
+import { getAllSkeins } from '../../../src/controllers/skeinController';
+import { Skein } from '../../../src/models/skeinModel';
+import { Request, Response } from 'express';
+
+jest.mock('../../../src/models/skeinModel');
+jest.mock('../../../src/aws/s3Service');
+
+const request = {} as Request;
+const response = {
+  status: jest.fn().mockReturnThis(),
+  json: jest.fn(),
+} as unknown as Response;
 
 describe('Getting all skeins', () => {
-  it('returns empty array when no skeins', async () => {});
+  afterEach(() => {
+    jest.clearAllMocks(); // Ensure mocks are cleared between tests
+  });
+  it('returns empty array when no skeins', async () => {
+    (Skein.find as jest.Mock).mockResolvedValue([]);
+    await getAllSkeins(request, response);
+
+    expect(Skein.find).toHaveBeenCalled();
+    expect(response.status).toHaveBeenCalledWith(200);
+    expect(response.json).toHaveBeenCalledWith({
+      status: 'success',
+      results: 0,
+      data: { skeinsWithImageUrl: [] },
+    });
+  });
   it('returns skeins when skeins', async () => {});
   it('attaches imageUrl when image included in req', async () => {});
   it('attaches imageUrl when no image included in req', async () => {});

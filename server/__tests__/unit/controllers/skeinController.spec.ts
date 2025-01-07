6ebb68542488da4fce82dc538ad8d2c189dcee14
Kirsten Mork <kirstenmork@hotmail.com>
2025-01-06 22:29:08 -0800

    Skein Unit Tests: Add test for default image
    If no image is added, we map to a default image in S3.
    
    Let's make sure that mapping occurs.

--- server/__tests__/unit/controllers/skeinController.spec.ts
+++ server/__tests__/unit/controllers/skeinController.spec.ts
@@ -30,7 +30,7 @@ describe('Getting all skeins', () => {
       data: { skeinsWithImageUrl: [] },
     });
   });
-  it('returns skeins when skeins', async () => {
+  it('returns skeins when skeins have images', async () => {
     const mockSkeins = [
       { color: 'Red', image: 'image1.png' },
       { color: 'Blue', image: 'image2.png' },
@@ -62,6 +62,40 @@ describe('Getting all skeins', () => {
       },
     });
   });
-  it('attaches imageUrl when image included in req', async () => {});
-  it('attaches imageUrl when no image included in req', async () => {});
+  it('attaches imageUrl with skeins without images', async () => {
+    // One skein does not have an image provided
+    const mockSkeins = [
+      { color: 'Red', image: 'image1.png' },
+      { color: 'Blue' },
+    ];
+    (Skein.find as jest.Mock).mockImplementationOnce(() => ({
+      lean: jest.fn().mockReturnValue(mockSkeins),
+    }));
+
+    (getSignedUrlFromS3 as jest.Mock).mockImplementation(
+      (bucket, key) => `https://mock-s3.com/${key}`,
+    );
+
+    await getAllSkeins(request, response);
+
+    expect(Skein.find).toHaveBeenCalled();
+    expect(getSignedUrlFromS3).toHaveBeenCalledTimes(mockSkeins.length);
+    expect(response.status).toHaveBeenCalledWith(200);
+
+    // Note: mongoose adds _doc property to bundle up the skein data,
+    // but we don't need that in the unit tests
+    expect(response.json).toHaveBeenCalledWith({
+      status: 'success',
+      results: 2,
+      data: {
+        skeinsWithImageUrl: [
+          { ...mockSkeins[0], imageUrl: 'https://mock-s3.com/image1.png' },
+          {
+            ...mockSkeins[1],
+            imageUrl: 'https://mock-s3.com/skeins/no-image.png', // default image attached
+          },
+        ],
+      },
+    });
+  });
 });

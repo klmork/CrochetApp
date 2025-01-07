06872e02c58e2e773e9562aed257601f77105002
Kirsten Mork <kirstenmork@hotmail.com>
2025-01-06 18:36:17 -0800

    Server:UnitTests: First outline
    These are the tests I want.  Let's mock the db call and the s3 call
    so we can implement this.

---
+++ server/__tests__/unit/controllers/skeinController.spec.ts
@@ -0,0 +1,9 @@
+// import { getAllSkeins, addSkien } from '../../controllers/skeinController';
+// import { Skein } from '../../models/skeinModel';
+
+describe('Getting all skeins', () => {
+  it('returns empty array when no skeins', async () => {});
+  it('returns skeins when skeins', async () => {});
+  it('attaches imageUrl when image included in req', async () => {});
+  it('attaches imageUrl when no image included in req', async () => {});
+});
